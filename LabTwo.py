2951ae1b08c5bcd0cce736d963c91aef35ab8c66
otheca3 <oscar7zheng@gmail.com>
2021-06-16 09:44:35 -0700

    var types practice

--- LabTwo.py
+++ LabTwo.py
@@ -1,1 +1,19 @@
 print ('hello world')
+
+#Step 1
+name = 'Lebron'
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = '23'
+
+#Step 2
+car_name = 'Ford'
+x = 50
+my_first_name = 'John'
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
