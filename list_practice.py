10ac3a757e14ad40a6ad95b3cff56ed7f47f936f
otheca3 <oscar7zheng@gmail.com>
2021-06-17 12:35:22 -0700

    list practice with some functions too

--- list_practice.py
+++ list_practice.py
@@ -39,3 +39,39 @@ three_pc_parts = pc_parts[0:4]
 print()
 print(three_cities)
 print(three_pc_parts)
+
+#Step 5: Changing elements in a list
+
+cities[0] = 'San Francisco'
+cities[2] = 'Brooklyn'
+cities[-3] = 'Hollywood'
+cities[5] = 'Tampa'
+
+#Step 6: Adding things to a list
+cities.append('Oakland')
+cities.extend(['New York City', 'Los Angeles'])
+cities.insert(0, 'Miami')
+
+#Step 7: Deleting elements from a list
+del cities[1]
+cities.pop(3)
+cities.remove('Tampa')
+
+
+def check_list(list1):
+    for element in list1:
+        print(element)
+
+    return 'Done'
+
+check_list(cities)
+
+def word_length_sort(list1):
+    temp_dict = {}
+    for i in range(len(list1)):
+        temp_dict[i] = len(list1[i])
+    
+    return temp_dict
+
+print()
+print(word_length_sort(cities))
