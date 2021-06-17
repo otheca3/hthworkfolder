083cb33fdc0bd530b70977ffca46846387fada52
otheca3 <oscar7zheng@gmail.com>
2021-06-17 12:14:03 -0700

    dictionary practice with loop functions

--- dict_practice.py
+++ dict_practice.py
@@ -60,3 +60,61 @@ del inventory['Foamposite']
 
 print()
 print(inventory)
+
+
+#Lab 4 Stuff
+def total_price(item1, item2):
+    price1 = meat_products[item1]
+    price2 = meat_products[item2]
+
+    total = price1 + price2
+    return total
+
+beef_cheese_price = total_price('Beef', 'Cheese')
+print()
+print(beef_cheese_price)
+
+def price_difference(item1, item2):
+    price1 = meat_products[item1]
+    price2 = meat_products[item2]
+
+    price_diff = abs(price1 - price2)
+    return price_diff
+
+beef_cheese_diff = price_difference('Beef', 'Cheese')
+print()
+print(beef_cheese_diff)
+
+def restock(item, multiplier):
+    old_stock = inventory[item]
+    new_stock = old_stock * multiplier
+
+    inventory[item] = new_stock
+    return inventory
+
+print()
+print(restock('Vans', 3))
+
+def clearance(item, divider):
+    old_stock = inventory[item]
+    new_stock = old_stock // divider
+
+    inventory[item] = new_stock
+    return inventory
+
+print()
+print(clearance('SB Dunk', 3))
+
+bugs_in_my_house = {
+    'fly' : 10,
+    'roach' : 0,
+    'crickets' : 1,
+    'me' : 1,
+}
+
+def update_bugs(bug, num):
+    bugs_in_my_house[bug] += num
+    return bugs_in_my_house
+
+print()
+print(update_bugs('me', 3))
